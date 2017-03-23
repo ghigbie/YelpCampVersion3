e2d86461bc4cf8b2c23802c1acd1fb0bbea7349c
ghigbie <gehigbie@gmail.com>
2017-03-23 21:04:53 +0000

    add filler data

--- seeds.js
+++ seeds.js
@@ -1,13 +1,36 @@
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 
+var data = [
+    {
+        name: "Cloud's Rest",
+        image: "http://i.imgur.com/nGSZnHN.jpg",
+        description: "A nice place to camp with great sky views"
+    },
+    {
+        name: "Granite Mountain's Rest",
+        image: "https://s-media-cache-ak0.pinimg.com/originals/9a/85/d8/9a85d8b22e42dd423da0afce92b43d28.jpg",
+        description: "A nice place to camp with great mountain views"
+    },
+    {
+        name: "Lake's Rest",
+        image: "http://www.lake-lewisville.org/wp-content/uploads/2012/07/Sycamore-Bend-Campgrounds-2.jpg",
+        description: "A nice place to camp with great mountain views"
+    },
+    ];
+
 function seedDB(){
+    //remove all campgrounds
     Campground.remove({}, function(err){
         if(err){
             console.log(err);
         }
         console.log("removed campgrounds");
     });
+    //add a few campgrounds
+    Campground.create
+    
+    //add a few comments
 }
 
-module.exports = seedDB();
+module.exports = seedDB;    
