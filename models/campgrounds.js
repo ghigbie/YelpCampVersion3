c970bb656f4a02d69df4fe01ebe6ac97b47e5f5b
ghigbie <gehigbie@gmail.com>
2017-03-23 06:19:51 +0000

    move code from app.js to models

---
+++ models/campgrounds.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
