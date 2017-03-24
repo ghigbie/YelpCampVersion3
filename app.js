ccab0288b8540fb23eeb055241b0c33b75d2c8a6
ghigbie <gehigbie@gmail.com>
2017-03-24 05:36:48 +0000

    refactor code in show

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var express     = require("express"),
     Campground  = require("./models/campground"),
     seedDB      = require("./seeds.js");
 
-seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp_v3");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+seedDB();
     
 app.get("/", function(req, res){
     res.render("landing");
